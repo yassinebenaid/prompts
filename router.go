1588032b6fdbeb76f5e279f35ada60f8dcf37110
yassinebenaid <yassinebenaide3@gmail.com>
2023-07-21 15:00:38 +0100

    seperate dispatchers

--- router.go
+++ router.go
@@ -9,28 +9,37 @@ import (
 
 type Router struct {
 	ran       bool
-	routes    map[string]Route
+	routes    map[string]*Route
 	groups    map[string]func(*Router)
 	fallback  func(*Context)
 	arguments []string
 	err       error
 }
 
-// Adds new route to the router ,
+// create new router instance
+func NewRouter() *Router {
+	return &Router{
+		ran:    false,
+		routes: make(map[string]*Route),
+		groups: make(map[string]func(*Router)),
+		fallback: func(*Context) {
+		},
+		arguments: os.Args[1:],
+		err:       nil,
+	}
+}
+
+// Adds new handler to the router ,
 // prefix is the first value after the program name ,
 // if Add called in a group , prefix is the first value after the group prefix
 func (r *Router) Add(prefix string, handler func(*Context)) *Router {
-	return r.AddRoute(prefix, Route{prefix: prefix, Handler: handler})
+	return r.AddRoute(prefix, &Route{prefix: prefix, Handler: handler})
 }
 
 // Adds new route to the router ,
 // prefix is the first value after the program name ,
 // if Add called in a group , prefix is the first value after the group prefix
-func (r *Router) AddRoute(prefix string, route Route) *Router {
-	if r.routes == nil {
-		r.routes = make(map[string]Route)
-	}
-
+func (r *Router) AddRoute(prefix string, route *Route) *Router {
 	prefix = strings.TrimSpace(prefix)
 
 	if !validPrefix(prefix) {
@@ -57,10 +66,6 @@ func (r *Router) AddRoute(prefix string, route Route) *Router {
 //
 // handler recieves a Router instance to register your sub routes
 func (r *Router) Group(prefix string, handler func(*Router)) *Router {
-	if r.groups == nil {
-		r.groups = make(map[string]func(*Router))
-	}
-
 	prefix = strings.TrimSpace(prefix)
 
 	if !validPrefix(prefix) {
@@ -99,32 +104,20 @@ func (r *Router) Dispatch() error {
 	r.ran = true
 
 	if len(r.arguments) < 1 {
-		if r.fallback != nil {
-			r.fallback(getContext(r.arguments))
-		}
+		r.fallback(getContext(r.arguments))
 		return nil
 	}
 
 	g, ok := r.groups[r.arguments[0]]
 
 	if ok {
-		r2 := Router{arguments: r.arguments[1:], ran: true}
-		g(&r2)
-		r2.ran = false
-		return r2.Dispatch()
+		return r.dispatchGroup(g)
 	}
 
 	route, ok := r.routes[r.arguments[0]]
 
 	if ok {
-		ctx := getContext(r.arguments[1:])
-		err := route.match(ctx)
-
-		if err != nil {
-			return err
-		}
-
-		route.Handler(ctx)
+		r.dispatchHandler(route)
 		return nil
 	}
 
@@ -133,17 +126,35 @@ func (r *Router) Dispatch() error {
 		return nil
 	}
 
-	return errors.New("undefined option : " + r.arguments[0])
+	return errors.New("undefined command : " + r.arguments[0])
 }
 
-// create new router instance
-func NewRouter() *Router {
-	return &Router{
-		arguments: os.Args[1:],
+func (r *Router) dispatchGroup(g func(*Router)) error {
+	r2 := &Router{
+		ran:       true,
+		routes:    make(map[string]*Route),
+		groups:    make(map[string]func(*Router)),
+		fallback:  func(*Context) {},
+		arguments: r.arguments[1:],
 	}
+	g(r2)
+	r2.ran = false
+	return r2.Dispatch()
+}
+
+func (r *Router) dispatchHandler(route *Route) error {
+	ctx := getContext(r.arguments[1:])
+	err := route.match(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	route.Handler(ctx)
+	return nil
 }
 
 func validPrefix(p string) bool {
-	rx := regexp.MustCompile(`^[A-z0-9\-\_]+$`)
+	rx := regexp.MustCompile(`^[A-z0-9\-]+$`)
 	return rx.MatchString(p)
 }
