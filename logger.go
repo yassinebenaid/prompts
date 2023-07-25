e58a406577587aa07f9e9a99902bd8c2aaee9ba6
yassinebenaid <yassinebenaide3@gmail.com>
2023-07-25 16:48:53 +0100

    added caller file to logger

--- logger.go
+++ logger.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"regexp"
+	"runtime"
 	"strings"
 	"time"
 )
@@ -18,39 +19,46 @@ type Logger interface {
 }
 
 type Log struct {
-	Writer io.Writer
+	Writer     io.Writer
+	WithCaller bool
 }
 
 func (log *Log) Info(m string, args ...any) {
-	log.log(Sprint(" INFO  ", T_BrightCyan, Bold), m, args...)
+	log.log(Sprint("INFO ", T_BrightCyan, Bold), m, args...)
 }
 
 func (log *Log) Debug(m string, args ...any) {
-	log.log(Sprint(" DEBUG ", T_BrightMagenta, Bold), m, args...)
+	log.log(Sprint("DEBUG", T_BrightMagenta, Bold), m, args...)
 
 }
 
 func (log *Log) Warn(m string, args ...any) {
-	log.log(Sprint(" WARN  ", T_BrightYellow, Bold), m, args...)
+	log.log(Sprint("WARN ", T_BrightYellow, Bold), m, args...)
 
 }
 
 func (log *Log) Error(m string, args ...any) {
-	log.log(Sprint(" ERROR ", T_BrightRed, Bold), m, args...)
+	log.log(Sprint("ERROR", T_BrightRed, Bold), m, args...)
 
 }
 
 func (log *Log) Fatal(m string, args ...any) {
-	label := SprintRGB(" FATAL ", 255, 0, 150)
+	label := SprintRGB("FATAL", 255, 0, 150)
 	log.log(Sprint(label, Bold), m, args...)
 	os.Exit(1)
 }
 
 func (log *Log) log(label string, m string, args ...any) {
 	time := time.Now().Format(time.DateTime)
-	fmt.Println(time, label, m, log.kvpair(args))
+	caller := ""
+
+	if log.WithCaller {
+		caller = log.getfl()
+	}
+
+	fmt.Println(time, label, caller, m, log.kvpair(args))
 	if log.Writer != nil {
-		log.Writer.Write(log.sanitize(time, label, m, log.kvpair(args)))
+		log.Writer.Write(log.sanitize(time, label, caller, m, log.kvpair(args)))
 	}
 }
 
@@ -66,10 +74,12 @@ func (log *Log) kvpair(kv []any) string {
 		out += Sprint(fmt.Sprint(kv[index])+"=", T_BrightBlack)
 
 		switch true {
-		case isNumeric(kv[index+1]) || isBool(kv[index+1]):
-			out += Sprint(fmt.Sprint(kv[index+1])+" ", Dim)
+		case isNumeric(kv[index+1]):
+			out += SprintRGB(fmt.Sprint(kv[index+1])+" ", 20, 160, 200)
+		case isBool(kv[index+1]):
+			out += SprintRGB(fmt.Sprint(kv[index+1])+" ", 225, 110, 30)
 		default:
-			out += Sprint(`"`+fmt.Sprint(kv[index+1])+`" `, Dim)
+			out += SprintRGB(`"`+fmt.Sprint(kv[index+1])+`" `, 100, 230, 80)
 		}
 		index += 2
 	}
@@ -90,4 +100,23 @@ func (log *Log) sanitize(s ...string) []byte {
 	str := strings.Join(s, " ")
 	str = regexp.MustCompile(`((\x1b)|(\033))\[[^m]+m`).ReplaceAllString(str, "")
 	return []byte(str + "\n")
+}
+
+func (log *Log) getfl() string {
+	_, f, l, ok := runtime.Caller(3)
+
+	if !ok {
+		return ""
+	}
+
+	wd, err := os.Getwd()
+
+	if err != nil {
+		return ""
+	}
+
+	f = strings.Replace(f, wd+"/", "", 1)
+	f = strings.Replace(f, wd, "", 1)
+
+	return Sprint("<"+f+":"+fmt.Sprint(l)+">", T_BrightBlack)
 }
